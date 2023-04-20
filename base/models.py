620ecaa810fc854a402a3738dbe39a08c7cb304d
Ezel <zellai022890@gmail.com>
2023-04-20 17:49:00 +0800

    view host username

--- base/models.py
+++ base/models.py
@@ -19,7 +19,7 @@ class Room(models.Model):
     update = models.DateTimeField(auto_now=True)
     created = models.DateTimeField(auto_now_add=True)
 
-    def __str__(self):
+    def __str__(self): 
         return self.name
     
 class Message(models.Model):
