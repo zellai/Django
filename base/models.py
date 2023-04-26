15c880a878f305f927ff7f004866b4fbdd43802b
Ezel <zellai022890@gmail.com>
2023-04-26 14:42:09 +0800

    Ordering- Ascending

--- base/models.py
+++ base/models.py
@@ -19,6 +19,9 @@ class Room(models.Model):
     update = models.DateTimeField(auto_now=True)
     created = models.DateTimeField(auto_now_add=True)
 
+    class Meta:
+        ordering = ['-update','-created']
+
     def __str__(self): 
         return self.name
     
