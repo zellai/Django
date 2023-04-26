94787ba3f8e43344ea9158909f58988d73b06889
Ezel <zellai022890@gmail.com>
2023-04-26 14:25:41 +0800

    CRUD

--- base/views.py
+++ base/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Room
+from .forms import RoomForm
 
 
 # Create your views here.
@@ -19,3 +20,8 @@ def room(request, pk):
     room = Room.objects.get(id=pk)
     context = {'room': room}
     return render(request, 'base/room.html', context)
+
+def createRoom(request):
+    form = RoomForm
+    context = {'form':form}
+    return render(request, 'base/room_form.html', context)
