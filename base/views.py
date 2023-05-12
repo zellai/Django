711f801c06ed15f7878670a64990e62bee167c97
Ezel <zellai022890@gmail.com>
2023-05-12 19:11:09 +0800

    Filter the topic

--- base/views.py
+++ base/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from .models import Room
+from .models import Room, Topic
 from .forms import RoomForm
 
 
@@ -12,8 +12,12 @@ from .forms import RoomForm
 # ]
 
 def home(request):
-    rooms = Room.objects.all()
-    context = {'rooms':rooms}
+    q = request.GET.get('q') if request.GET.get('q') != None else ''
+
+    rooms = Room.objects.filter(topic__name__icontains=q)
+
+    topics = Topic.objects.all()
+    context = {'rooms':rooms, 'topics': topics}
     return render(request, 'base/home.html', context)
 
 def room(request, pk):
