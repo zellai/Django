21b018774b801d90376f9c3a04b28abcc33be3fb
Ezel <zellai022890@gmail.com>
2023-05-17 11:53:55 +0800

    Register

--- base/views.py
+++ base/views.py
@@ -17,7 +17,7 @@ from .forms import RoomForm
 # ]
 
 def loginPage(request):
-    
+    page = 'login'
     if request.user.is_authenticated:
         return redirect('home')
     
@@ -38,12 +38,16 @@ def loginPage(request):
         else:
             messages.error(request, 'Username or Password does not exist.')
     
-    context = {}
+    context = {'page' : page }
     return render(request, 'base/login_register.html', context)
 
 def logoutUser(request):
     logout(request)
     return redirect('home')
+
+def registerPage(request):
+    page = 'register'
+    return render(request, 'base/login_register.html')
 
 def home(request):
     q = request.GET.get('q') if request.GET.get('q') != None else ''
