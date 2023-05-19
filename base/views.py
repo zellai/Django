c2fec2b8e1c5d6d22972abb073b3c42f23ad430d
Ezel <zellai022890@gmail.com>
2023-05-19 17:11:08 +0800

    Adding participants in views

--- base/views.py
+++ base/views.py
@@ -79,6 +79,7 @@ def home(request):
 def room(request, pk):
     room = Room.objects.get(id=pk)
     room_messages = room.message_set.all().order_by('-created')
+    participants = room.participants.all()
     
     if request.method == "POST":
         message = Message.objects.create(
@@ -86,8 +87,9 @@ def room(request, pk):
             room=room,
             body=request.POST.get('body')
         )
+        return redirect('room', pk=room.id)
     
-    context = {'room': room, 'room_messages': room_messages}
+    context = {'room': room, 'room_messages': room_messages, 'participants': participants}
     return render(request, 'base/room.html', context)
 
 @login_required(login_url='login')
